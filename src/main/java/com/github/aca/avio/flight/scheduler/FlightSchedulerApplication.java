7708a4f2e308da17e2475b76f9a2970e252ad251
Mariel Latella Barros <mariel.latellabarros@acagroup.be>
2025-03-17 22:23:40 +0100

    Initial project setup

---
+++ src/main/java/com/github/aca/avio/flight/scheduler/FlightSchedulerApplication.java
@@ -0,0 +1,13 @@
+package com.github.aca.avio.flight.scheduler;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class FlightSchedulerApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(FlightSchedulerApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/github/aca/avio/flight/scheduler/FlightSchedulerApplicationTests.java
@@ -0,0 +1,13 @@
+package com.github.aca.avio.flight.scheduler;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class FlightSchedulerApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
