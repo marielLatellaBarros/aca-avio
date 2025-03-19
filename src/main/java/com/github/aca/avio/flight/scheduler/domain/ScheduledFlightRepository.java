b4a48ed4fc2279f237a4c96e86cf712eeac7e763
Mariel Latella Barros <mariel.latellabarros@acagroup.be>
2025-03-19 22:57:36 +0100

    code cleanup

--- src/main/java/com/github/aca/avio/flight/scheduler/domain/ScheduledFlightRepository.java
+++ src/main/java/com/github/aca/avio/flight/scheduler/domain/ScheduledFlightRepository.java
@@ -1,16 +1,14 @@
 package com.github.aca.avio.flight.scheduler.domain;
 
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.JpaSpecificationExecutor;
 
-import java.util.List;
 import java.util.Optional;
 import java.util.UUID;
 
-public interface ScheduledFlightRepository extends JpaRepository<ScheduledFlight, Integer> {
+public interface ScheduledFlightRepository extends JpaRepository<ScheduledFlight, Integer>, JpaSpecificationExecutor<ScheduledFlight> {
 
     Optional<ScheduledFlight> findByUuid(UUID uuid);
 
     void deleteByUuid(UUID uuid);
-
-    List<ScheduledFlight> findByFlightNumber(String flightNumber);
 }
