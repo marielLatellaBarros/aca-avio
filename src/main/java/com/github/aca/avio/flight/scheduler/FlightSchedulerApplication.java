11ea496453389c4187f238c0c4d3b2111c07dfb7
Mariel Latella Barros <mariel.latellabarros@acagroup.be>
2025-03-19 10:06:58 +0100

    added support for mapping objects and validating properties

--- src/main/java/com/github/aca/avio/flight/scheduler/FlightSchedulerApplication.java
+++ src/main/java/com/github/aca/avio/flight/scheduler/FlightSchedulerApplication.java
@@ -1,13 +1,20 @@
 package com.github.aca.avio.flight.scheduler;
 
+import org.modelmapper.ModelMapper;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.annotation.Bean;
 
 @SpringBootApplication
 public class FlightSchedulerApplication {
 
 	public static void main(String[] args) {
 		SpringApplication.run(FlightSchedulerApplication.class, args);
+	}
+
+	@Bean
+	public ModelMapper modelMapper() {
+		return new ModelMapper();
 	}
 
 }
